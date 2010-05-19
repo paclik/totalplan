927934d59b81f73d2018f2554c1ffd04db66d77c
amelem <amelem@gmail.com>
2010-05-19 15:02:36 +0200

    ok

---
+++ db/migrate/20100517131024_create_contacts.rb
@@ -0,0 +1,21 @@
+class CreateContacts < ActiveRecord::Migration
+  def self.up
+    create_table :contacts do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :mob_phone
+      t.string :land_line
+      t.string :email
+      t.decimal :height
+      t.decimal :weight
+      t.decimal :age
+      t.string :body_type
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :contacts
+  end
+end

8b394c1cba3a2bd9002532d3648abe66465d10fa
amelem <amelem@gmail.com>
2010-05-20 10:38:58 +0200

    zmena modelu contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ActiveRecord::Base
   validates_presence_of :last_name, :first_name
   validates_numericality_of :height, :weight, :age
-  
+  validates_numericality_of :age, :greater_than => 14, :less_than => 132
 end

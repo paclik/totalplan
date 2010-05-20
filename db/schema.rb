3c5be684ae4ddfc4ce0aaa5f67908ad8e53ef0de
amelem <amelem@gmail.com>
2010-05-20 10:39:52 +0200

    zmena modelu contacts

---
+++ db/schema.rb
@@ -0,0 +1,62 @@
+# This file is auto-generated from the current state of the database. Instead of editing this file, 
+# please use the migrations feature of Active Record to incrementally modify your database, and
+# then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your database schema. If you need
+# to create the application database on another system, you should be using db:schema:load, not running
+# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20100517131024) do
+
+  create_table "coaches", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "contacts", :force => true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "mob_phone"
+    t.string   "land_line"
+    t.string   "email"
+    t.integer  "height",     :limit => 10, :precision => 10, :scale => 0
+    t.integer  "weight",     :limit => 10, :precision => 10, :scale => 0
+    t.integer  "age",        :limit => 10, :precision => 10, :scale => 0
+    t.string   "body_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "players", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "players_sports", :force => true do |t|
+    t.integer  "player_id"
+    t.integer  "sport_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "sports", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "coach_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tools", :force => true do |t|
+    t.string   "name"
+    t.integer  "sport_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
